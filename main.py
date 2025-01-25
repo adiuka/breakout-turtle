c2ef7e0a4628a320874c62d4da0f8c95237202e5
adiuka <amartinaitiz@gmail.com>
2025-01-25 09:16:48 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from turtle import Screen
+import time
+
+
+screen = Screen()
+screen.setup(800, 600)
+screen.bgcolor("black")
+screen.title("turtle_breakout")
+screen.tracer(0)
+
+
+
+
+screen.exitonclick()
